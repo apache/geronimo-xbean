b8ef3206c13ae3159a618defb3d1cc0d565444a6
James Strachan <jstrachan@apache.org>
2005-09-15 21:05:06 +0000

    initial import of the shell of the spring module to overload the XML parsing in Spring to allow custom XML dialects etc
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380375 13f79535-47bb-0310-9956-ffa450edef68

---
+++ spring/src/java/org/xbean/spring/context/FileSystemXmlApplicationContext.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.xbean.spring.context;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.xbean.spring.context.impl.XBeanXmlBeanDefinitionParser;
+
+/**
+ * An XBean version of the regular Spring class to provide improved XML
+ * handling.
+ * 
+ * @author James Strachan
+ * @version $Revision: 1.1 $
+ */
+public class FileSystemXmlApplicationContext extends
+        org.springframework.context.support.FileSystemXmlApplicationContext {
+
+    public FileSystemXmlApplicationContext(String arg0) throws BeansException {
+        super(arg0);
+    }
+
+    public FileSystemXmlApplicationContext(String[] arg0, ApplicationContext arg1) throws BeansException {
+        super(arg0, arg1);
+    }
+
+    public FileSystemXmlApplicationContext(String[] arg0, boolean arg1, ApplicationContext arg2) throws BeansException {
+        super(arg0, arg1, arg2);
+    }
+
+    public FileSystemXmlApplicationContext(String[] arg0, boolean arg1) throws BeansException {
+        super(arg0, arg1);
+    }
+
+    public FileSystemXmlApplicationContext(String[] arg0) throws BeansException {
+        super(arg0);
+    }
+
+    protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
+        super.initBeanDefinitionReader(reader);
+        reader.setParserClass(XBeanXmlBeanDefinitionParser.class);
+    }
+}
---
+++ spring/src/java/org/xbean/spring/context/XmlWebApplicationContext.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.xbean.spring.context;
+
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.xbean.spring.context.impl.XBeanXmlBeanDefinitionParser;
+
+/**
+ * An XBean version of the regular Spring class to provide improved XML
+ * handling.
+ * 
+ * @author James Strachan
+ * @version $Revision: 1.1 $
+ */
+public class XmlWebApplicationContext extends org.springframework.web.context.support.XmlWebApplicationContext {
+
+    public XmlWebApplicationContext() {
+    }
+
+    protected void initBeanDefinitionReader(XmlBeanDefinitionReader reader) {
+        super.initBeanDefinitionReader(reader);
+        reader.setParserClass(XBeanXmlBeanDefinitionParser.class);
+    }
+}
