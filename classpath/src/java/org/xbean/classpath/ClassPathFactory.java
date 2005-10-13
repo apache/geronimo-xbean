8adddeae89fc1de5ba47c936a3b728e718d37328
David Blevins <dblevins@apache.org>
2005-10-13 05:48:35 +0000

    The beginning of code to manipulate classloaders and whatnot.
    The test case that's in there now needs to get updated to maybe use
    some of Dain's magic that creates a class, then we could avoid the download.
    
    but it's just too funny the way it is now....
    
    
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380405 13f79535-47bb-0310-9956-ffa450edef68

---
+++ classpath/src/java/org/xbean/classpath/ClassPathFactory.java
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright 2005 David Blevins
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.xbean.classpath;
+
+public class ClassPathFactory {
+    public static ClassPath createClassPath(String name){
+        if (name.equalsIgnoreCase("tomcat")) {
+            return new TomcatClassPath();
+        } else if (name.equalsIgnoreCase("tomcat-common")) {
+            return new TomcatClassPath();
+        } else if (name.equalsIgnoreCase("tomcat-webapp")) {
+            return new TomcatWebAppClassPath();
+        } else if (name.equalsIgnoreCase("bootstrap")) {
+            return new SystemClassPath();
+        } else if (name.equalsIgnoreCase("system")) {
+            return new SystemClassPath();
+        } else if (name.equalsIgnoreCase("thread")) {
+            return new ContextClassPath();
+        } else if (name.equalsIgnoreCase("context")) {
+            return new ContextClassPath();
+        } else {
+            return new ContextClassPath();
+        }
+    }
+}
---
+++ classpath/src/java/org/xbean/classpath/SystemClassPath.java
@@ -0,0 +1,80 @@
+/**
+ *
+ * Copyright 2005 David Blevins
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.xbean.classpath;
+
+import java.net.URLClassLoader;
+import java.net.URL;
+import java.io.File;
+
+public class SystemClassPath extends SunURLClassPath {
+
+    private URLClassLoader sysLoader;
+
+    public void addJarsToPath(File dir) throws Exception {
+        this.addJarsToPath(dir, getSystemLoader());
+        this.rebuildJavaClassPathVariable();
+    }
+
+    public void addJarToPath(URL jar) throws Exception {
+        //System.out.println("[|] SYSTEM "+jar.toExternalForm());
+        this.addJarToPath(jar, getSystemLoader());
+        this.rebuildJavaClassPathVariable();
+    }
+
+    public ClassLoader getClassLoader() {
+        try {
+            return getSystemLoader();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private URLClassLoader getSystemLoader() throws Exception {
+        if (sysLoader == null) {
+            sysLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+        }
+        return sysLoader;
+    }
+
+    private void rebuildJavaClassPathVariable() throws Exception {
+        sun.misc.URLClassPath cp = getURLClassPath(getSystemLoader());
+        URL[] urls = cp.getURLs();
+        //for (int i=0; i < urls.length; i++){
+        //    System.out.println(urls[i].toExternalForm());
+        //}
+        if (urls.length < 1)
+            return;
+
+        StringBuffer path = new StringBuffer(urls.length * 32);
+
+        File s = new File(urls[0].getFile());
+        path.append(s.getPath());
+        //System.out.println(s.getPath());
+
+        for (int i = 1; i < urls.length; i++) {
+            path.append(File.pathSeparator);
+
+            s = new File(urls[i].getFile());
+            //System.out.println(s.getPath());
+            path.append(s.getPath());
+        }
+        try {
+            System.setProperty("java.class.path", path.toString());
+        } catch (Exception e) {
+        }
+    }
+}
