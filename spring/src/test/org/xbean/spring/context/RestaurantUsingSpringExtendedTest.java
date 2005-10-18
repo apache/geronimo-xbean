46b14d4a5f20788278d4468760f5d3889815b4ce
Guillaume Nodet <gnodet@apache.org>
2005-10-18 19:21:37 +0000

    Add qname parsing on regular spring beans and parsing of root elements
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380439 13f79535-47bb-0310-9956-ffa450edef68

---
+++ spring/src/test/org/xbean/spring/context/RestaurantUsingSpringExtendedTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.xbean.spring.context;
+
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.xbean.spring.example.PizzaService;
+import org.xbean.spring.example.RestaurantService;
+
+import java.util.List;
+
+/**
+ * @author James Strachan
+ * @version $Id$
+ * @since 2.0
+ */
+public class RestaurantUsingSpringExtendedTest extends RestaurantUsingSpringTest {
+
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("org/xbean/spring/context/restaurant-spring-extended.xml");
+    }
+}
