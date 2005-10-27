9cd768cf0a204567dc2ad459af01de57658a9643
Guillaume Nodet <gnodet@apache.org>
2005-10-27 16:05:09 +0000

    Fix problem when parsing a child bean defined with spring schema namespace. Add testcase for root bean parsing.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380446 13f79535-47bb-0310-9956-ffa450edef68

---
+++ spring/src/test/org/xbean/spring/context/RestaurantUsingXBeanAsRootTest.java
@@ -0,0 +1,10 @@
+package org.xbean.spring.context;
+
+import org.springframework.context.support.AbstractXmlApplicationContext;
+
+public class RestaurantUsingXBeanAsRootTest extends RestaurantUsingSpringTest {
+
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("org/xbean/spring/context/restaurant-xbean-root.xml");
+    }
+}
