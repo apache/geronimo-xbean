2d3c9e1f70fcbbaae0a37cebda8b6efa41b6f6ca
Guillaume Nodet <gnodet@apache.org>
2006-01-21 13:43:14 +0000

    Fix LogicBlaze copyrights
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380530 13f79535-47bb-0310-9956-ffa450edef68

--- spring/src/test/java/org/apache/xbean/spring/jndi/JndiTest.java
+++ spring/src/test/java/org/apache/xbean/spring/jndi/JndiTest.java
@@ -1,6 +1,6 @@
 /** 
  * 
- * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * Copyright 2005-2006 The Apache Software Foundation or its licensors,  as applicable.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
