5ddce5532284ddc9cba37a7d73f2ba4665e719e4
David Blevins <dblevins@apache.org>
2006-01-25 05:41:07 +0000

    Tests for factory methods
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380539 13f79535-47bb-0310-9956-ffa450edef68

--- reflect/src/test/java/org/apache/xbean/recipe/ObjectRecipeTest.java
+++ reflect/src/test/java/org/apache/xbean/recipe/ObjectRecipeTest.java
@@ -7,30 +7,41 @@ import junit.framework.TestCase;
 import org.apache.xbean.propertyeditor.PropertyEditors;
 
 import java.net.URL;
+import java.net.MalformedURLException;
 
 public class ObjectRecipeTest extends TestCase {
-    ObjectRecipe objectRecipe;
+
+    protected void setUp() throws Exception {
+        PropertyEditors.class.getName();
+    }
 
     public void testSetters() throws Exception {
-        PropertyEditors.class.getName();
-
-        Person expected = new Person("Joe", 21, new URL("http://www.acme.org"));
 
         ObjectRecipe objectRecipe = new ObjectRecipe(Person.class);
-        objectRecipe.setProperty("name", "Joe");
-        objectRecipe.setProperty("age", "21");
-        objectRecipe.setProperty("homePage", "http://www.acme.org");
-
-        Person actual = (Person) objectRecipe.create(Person.class.getClassLoader());
-        assertEquals("person", expected, actual);
+        doTest(objectRecipe);
     }
 
     public void testConstructor() throws Exception {
-        PropertyEditors.class.getName();
 
+        ObjectRecipe objectRecipe = new ObjectRecipe(Person.class, new String[]{"name", "age", "homePage"}, new Class[]{String.class, Integer.TYPE, URL.class});
+        doTest(objectRecipe);
+    }
+
+    public void testFactoryMethodAndSetters() throws Exception {
+
+        ObjectRecipe objectRecipe = new ObjectRecipe(Person.class, "newInstance");
+        doTest(objectRecipe);
+    }
+
+    public void testFactoryMethodWithParams() throws Exception {
+
+        ObjectRecipe objectRecipe = new ObjectRecipe(Person.class, "newInstance", new String[]{"name", "age", "homePage"}, new Class[]{String.class, Integer.TYPE, URL.class});
+        doTest(objectRecipe);
+    }
+
+    private void doTest(ObjectRecipe objectRecipe) throws Exception {
         Person expected = new Person("Joe", 21, new URL("http://www.acme.org"));
 
-        ObjectRecipe objectRecipe = new ObjectRecipe(Person.class,new String[]{"name","age","homePage"},new Class[]{String.class, Integer.TYPE, URL.class});
         objectRecipe.setProperty("name", "Joe");
         objectRecipe.setProperty("age", "21");
         objectRecipe.setProperty("homePage", "http://www.acme.org");
@@ -38,6 +49,5 @@ public class ObjectRecipeTest extends TestCase {
         Person actual = (Person) objectRecipe.create(Person.class.getClassLoader());
         assertEquals("person", expected, actual);
     }
-
 
 }
