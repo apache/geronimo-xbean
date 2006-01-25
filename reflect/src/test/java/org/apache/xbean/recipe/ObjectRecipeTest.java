23637fa3d7fee66fae11a74dde955eb8c40f637a
David Blevins <dblevins@apache.org>
2006-01-25 04:07:19 +0000

    Slightly improved test case
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@380537 13f79535-47bb-0310-9956-ffa450edef68

--- reflect/src/test/java/org/apache/xbean/recipe/ObjectRecipeTest.java
+++ reflect/src/test/java/org/apache/xbean/recipe/ObjectRecipeTest.java
@@ -12,12 +12,11 @@ public class ObjectRecipeTest extends TestCase {
 
     public void testObjectRecipe() throws Exception {
         PropertyEditors.class.getName();
-        Object value = PropertyEditors.getValue(Integer.TYPE, "21");
-        System.out.println("value = " + value);
 
         ObjectRecipe objectRecipe = new ObjectRecipe(Person.class);
         objectRecipe.setProperty("name","Joe");
         objectRecipe.setProperty("age", "21");
+        objectRecipe.setProperty("homePage", "http://www.acme.org");
 
         Person person = (Person) objectRecipe.create(Person.class.getClassLoader());
         System.out.println("person = " + person);
