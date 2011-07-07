af264cb08101eee215a56c72250cc6159422c92a
Jarek Gawor <gawor@apache.org>
2011-07-07 19:18:09 +0000

    XBEAN-177: DelegatingBundle optimization take 2. Minimize the use of PackageAdmin
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@1143986 13f79535-47bb-0310-9956-ffa450edef68

--- xbean-bundleutils/src/main/java/org/apache/xbean/osgi/bundle/util/DelegatingBundle.java
+++ xbean-bundleutils/src/main/java/org/apache/xbean/osgi/bundle/util/DelegatingBundle.java
@@ -27,8 +27,11 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
@@ -64,42 +67,92 @@ public class DelegatingBundle implements Bundle {
     }
     
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        for (Bundle bundle : bundles) {
-            try {
-                return bundle.loadClass(name);
-            } catch (ClassNotFoundException ex) {
-                int index = name.lastIndexOf('.');
-                if (index > 0) {
-                    /*
-                     * We got CNFE and if we are wired to a bundle that exports the 
-                     * given package then there is no hope of loading the class.
-                     */
-                    String packageName = name.substring(0, index);
-                    if (isWired(bundle, packageName)) {
+        try {
+            return bundle.loadClass(name);
+        } catch (ClassNotFoundException ex) {
+            int index = name.lastIndexOf('.');
+            if (index > 0) {
+                // see if there are any bundles exporting the package
+                String packageName = name.substring(0, index);
+                Set<Bundle> packageBundles = getPackageBundles(packageName);
+                if (packageBundles == null) {
+                    // package is NOT exported
+                    
+                    Iterator<Bundle> iterator = bundles.iterator();
+                    // skip first bundle
+                    iterator.next();
+                    // attempt to load the class from the remaining bundles
+                    while (iterator.hasNext()) {
+                        Bundle delegate = iterator.next();
+                        try {
+                            return delegate.loadClass(name);
+                        } catch (ClassNotFoundException e) {
+                            // ignore
+                        }
+                    }
+                    
+                    throw ex;
+                } else {
+                    // package is exported 
+                    
+                    // see if any of our bundles is wired to the exporter 
+                    Bundle delegate = findFirstBundle(packageBundles);
+                    if (delegate == null || delegate == bundle) {
+                        // nope. no static wires but might need to check for dynamic wires in the future.
                         throw ex;
                     } else {
-                        // ignore - try to load with next bundle
-                    }
-                } else {
-                    // no package name
-                    throw ex;
-                }                
-            }
+                        // yes. attempt to load the class from it
+                        return delegate.loadClass(name);
+                    } 
+                }
+            }  else {
+                // no package name
+                throw ex;
+            }  
         }
-        throw new ClassNotFoundException(name);
     }
-
-    private boolean isWired(Bundle importingBundle, String packageName) {
+    
+    private Set<Bundle> getPackageBundles(String packageName) {
         BundleContext context = bundle.getBundleContext();
         ServiceReference reference = context.getServiceReference(PackageAdmin.class.getName());
         PackageAdmin packageAdmin = (PackageAdmin) context.getService(reference);
+        Set<Bundle> bundles = null;
         try {
-            ExportedPackage[] packages = packageAdmin.getExportedPackages(packageName);
-            Bundle exportingBundle = BundleUtils.getWiredBundle(importingBundle, packages);
-            return (exportingBundle != null);
+            ExportedPackage[] exportedPackages = packageAdmin.getExportedPackages(packageName);
+            if (exportedPackages != null && exportedPackages.length > 0) {
+                bundles = new HashSet<Bundle>();
+                for (ExportedPackage exportedPackage : exportedPackages) {
+                    bundles.add(exportedPackage.getExportingBundle());
+                    Bundle[] importingBundles = exportedPackage.getImportingBundles();
+                    if (importingBundles != null) {
+                        for (Bundle importingBundle : importingBundles) {
+                            bundles.add(importingBundle);
+                        }
+                    }
+                }
+            }
+            return bundles;
         } finally {
             context.ungetService(reference);
         }
+    }
+    
+    private Bundle findFirstBundle(Set<Bundle> packageBundles) {
+        Collection<Bundle> c1 = bundles;
+        Collection<Bundle> c2 = packageBundles;
+        
+        if (bundles instanceof Set<?> && bundles.size() > packageBundles.size()) {
+            c1 = packageBundles;
+            c2 = bundles;
+        }
+        
+        for (Bundle bundle : c1) {
+            if (c2.contains(bundle)) {
+                return bundle;
+            }
+        }
+        
+        return null;
     }
     
     public URL getResource(String name) {
