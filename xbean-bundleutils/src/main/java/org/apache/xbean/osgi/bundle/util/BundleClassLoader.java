b52369787dc3a1ae1d3495318ead79bdc6e51594
Haihong Xu <xuhaihong@apache.org>
2011-08-31 07:37:38 +0000

    Revert XBEAN-183 Disable wired bundles search by default
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@1163514 13f79535-47bb-0310-9956-ffa450edef68

--- xbean-bundleutils/src/main/java/org/apache/xbean/osgi/bundle/util/BundleClassLoader.java
+++ xbean-bundleutils/src/main/java/org/apache/xbean/osgi/bundle/util/BundleClassLoader.java
@@ -27,20 +27,20 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleReference;
 
 /**
- * ClassLoader for a {@link Bundle}.
+ * ClassLoader for a {@link Bundle}. 
  * <br/>
- * In OSGi, resource lookup on resources in the <i>META-INF</i> directory using {@link Bundle#getResource(String)} or
- * {@link Bundle#getResources(String)} does not return the resources found in the wired bundles of the bundle
- * (wired via <i>Import-Package</i> or <i>DynamicImport-Package</i>). This class loader implementation provides
+ * In OSGi, resource lookup on resources in the <i>META-INF</i> directory using {@link Bundle#getResource(String)} or 
+ * {@link Bundle#getResources(String)} does not return the resources found in the wired bundles of the bundle 
+ * (wired via <i>Import-Package</i> or <i>DynamicImport-Package</i>). This class loader implementation provides 
  * {@link #getResource(String) and {@link #getResources(String)} methods that do delegate such resource lookups to
- * the wired bundles.
+ * the wired bundles. 
  * <br/>
- * The URLs returned by {@link Bundle#getResource(String)} or {@link Bundle#getResources(String)} methods are
+ * The URLs returned by {@link Bundle#getResource(String)} or {@link Bundle#getResources(String)} methods are 
  * OSGi framework specific &quot;bundle&quot; URLs. This sometimes can cause problems with 3rd party libraries
  * which do not understand how to interpret the &quot;bundle&quot; URLs. This ClassLoader implementation, if enabled,
  * can return <tt>jar</tt> URLs for resources found in embedded jars in the bundle. If a resource is found within a
  * directory in the bundle the URL returned for that resource is unconverted.
- *
+ * 
  * @version $Rev$ $Date$
  */
 public class BundleClassLoader extends ClassLoader implements DelegatingBundleReference {
@@ -49,17 +49,17 @@ public class BundleClassLoader extends ClassLoader implements DelegatingBundleRe
     protected final BundleResourceHelper resourceHelper;
 
     public BundleClassLoader(Bundle bundle) {
-        this(bundle,
-             BundleResourceHelper.getSearchWiredBundles(false),
+        this(bundle, 
+             BundleResourceHelper.getSearchWiredBundles(true), 
              BundleResourceHelper.getConvertResourceUrls(false));
     }
-
+        
     public BundleClassLoader(Bundle bundle, boolean searchWiredBundles) {
-        this(bundle,
-             searchWiredBundles,
+        this(bundle, 
+             searchWiredBundles, 
              BundleResourceHelper.getConvertResourceUrls(false));
     }
-
+    
     public BundleClassLoader(Bundle bundle, boolean searchWiredBundles, boolean convertResourceUrls) {
         this.bundle = bundle;
         this.resourceHelper = new BundleResourceHelper(bundle, searchWiredBundles, convertResourceUrls);
@@ -69,7 +69,7 @@ public class BundleClassLoader extends ClassLoader implements DelegatingBundleRe
     public String toString() {
         return "[BundleClassLoader] " + bundle;
     }
-
+    
     @Override
     protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
         Class clazz = bundle.loadClass(name);
@@ -78,48 +78,48 @@ public class BundleClassLoader extends ClassLoader implements DelegatingBundleRe
         }
         return clazz;
     }
-
+    
     @Override
     public URL getResource(String name) {
         return resourceHelper.getResource(name);
     }
-
+    
     @Override
     public Enumeration<URL> getResources(String name) throws IOException {
         return resourceHelper.getResources(name);
     }
-
+    
     @Override
     public Enumeration<URL> findResources (String name) throws IOException {
         return this.getResources(name);
     }
-
+    
     public void setSearchWiredBundles(boolean search) {
         resourceHelper.setSearchWiredBundles(search);
     }
-
+    
     public boolean getSearchWiredBundles() {
         return resourceHelper.getSearchWiredBundles();
     }
-
+           
     public void setConvertResourceUrls(boolean convert) {
         resourceHelper.setConvertResourceUrls(convert);
     }
-
+    
     public boolean getConvertResourceUrls() {
         return resourceHelper.getConvertResourceUrls();
     }
-
+    
     /**
      * Return the bundle associated with this classloader.
-     *
-     * In most cases the bundle associated with the classloader is a regular framework bundle.
+     * 
+     * In most cases the bundle associated with the classloader is a regular framework bundle. 
      * However, in some cases the bundle associated with the classloader is a {@link DelegatingBundle}.
      * In such cases, the <tt>unwrap</tt> parameter controls whether this function returns the
      * {@link DelegatingBundle} instance or the main application bundle backing with the {@link DelegatingBundle}.
      *
-     * @param unwrap If true and if the bundle associated with this classloader is a {@link DelegatingBundle},
-     *        this function will return the main application bundle backing with the {@link DelegatingBundle}.
+     * @param unwrap If true and if the bundle associated with this classloader is a {@link DelegatingBundle}, 
+     *        this function will return the main application bundle backing with the {@link DelegatingBundle}. 
      *        Otherwise, the bundle associated with this classloader is returned as is.
      * @return The bundle associated with this classloader.
      */
@@ -129,14 +129,14 @@ public class BundleClassLoader extends ClassLoader implements DelegatingBundleRe
         }
         return bundle;
     }
-
+    
     /**
      * Return the bundle associated with this classloader.
-     *
-     * This method calls {@link #getBundle(boolean) getBundle(true)} and therefore always returns a regular
-     * framework bundle.
+     * 
+     * This method calls {@link #getBundle(boolean) getBundle(true)} and therefore always returns a regular 
+     * framework bundle.  
      * <br><br>
-     * Note: Some libraries use {@link BundleReference#getBundle()} to obtain a bundle for the given
+     * Note: Some libraries use {@link BundleReference#getBundle()} to obtain a bundle for the given 
      * classloader and expect the returned bundle instance to be work with any OSGi API. Some of these API might
      * not work if {@link DelegatingBundle} is returned. That is why this function will always return
      * a regular framework bundle. See {@link #getBundle(boolean)} for more information.
