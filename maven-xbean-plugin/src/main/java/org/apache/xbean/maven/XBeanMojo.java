c7c69087578bf1a2594d6f356bc5d078a77f4f39
Hiram R. Chirino <chirino@apache.org>
2012-09-25 14:22:30 +0000

    Add support to the xbean maven mojo to 'include' multiple additional class directories
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@1389876 13f79535-47bb-0310-9956-ffa450edef68

--- maven-xbean-plugin/src/main/java/org/apache/xbean/maven/XBeanMojo.java
+++ maven-xbean-plugin/src/main/java/org/apache/xbean/maven/XBeanMojo.java
@@ -92,6 +92,11 @@ public class XBeanMojo extends AbstractMojo implements LogFacade {
     /**
      * @parameter
      */
+    private List<String> classPathIncludes;
+
+    /**
+     * @parameter
+     */
     private String excludedClasses;
 
     /**
@@ -262,6 +267,15 @@ public class XBeanMojo extends AbstractMojo implements LogFacade {
                                 + " to classpath");
                 urls.add(classPathElement.getFile().toURL());
             }
+
+            if( classPathIncludes!=null ) {
+                for (String include : classPathIncludes) {
+                    final URL url = new File(include).toURL();
+                    getLog().debug("Adding to classpath : " + url);
+                    urls.add(url);
+                }
+            }
+
             URLClassLoader appClassloader = new URLClassLoader(urls.toArray(new URL[urls.size()]), 
                     this.getClass().getClassLoader());
             return appClassloader;
