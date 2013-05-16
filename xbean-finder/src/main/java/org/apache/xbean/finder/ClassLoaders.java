555b2d832109deb6b766f8ab607445b4a5815bf5
David Blevins <dblevins@apache.org>
2013-05-16 19:56:42 +0000

    Tweak to XBEAN-243 to skip the ExtClassLoader and not bring in native libraries Better fix
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/xbean/trunk@1483537 13f79535-47bb-0310-9956-ffa450edef68

--- xbean-finder/src/main/java/org/apache/xbean/finder/ClassLoaders.java
+++ xbean-finder/src/main/java/org/apache/xbean/finder/ClassLoaders.java
@@ -31,7 +31,7 @@ public final class ClassLoaders {
     private static final ClassLoader SYSTEM = ClassLoader.getSystemClassLoader();
 
     public static Set<URL> findUrls(final ClassLoader classLoader) throws IOException {
-        if (classLoader == null || classLoader.getClass().getName().equals("sun.misc.Launcher$ExtClassLoader")) {
+        if (classLoader == null || (SYSTEM.getParent() != null && classLoader == SYSTEM.getParent())) {
             return Collections.emptySet();
         }
 
